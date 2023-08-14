ce6f232f5faa72a770f420fc13ebd5a857a1057a
MarcinPierchala <68757012+MarcinPierchala@users.noreply.github.com>
2023-08-14 07:47:36 +0200

    basic_solution_ip_written_on_the_console

---
+++ MyIpTry.py
@@ -0,0 +1,18 @@
+import time
+import selenium
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
+
+s = Service('C:\\Python311\\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+driver.get("http://checkip.dyndns.org/")
+
+driver.maximize_window()
+#time.sleep(5000)
+ip_message = driver.find_element("xpath", "/html/body").text
+print(ip_message.strip('Current IP Address: '))
+
+time.sleep(5000)
+
+
